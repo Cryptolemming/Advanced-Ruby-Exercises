e04363d0a4c6986913715399df22e720b802a41b
cryptolemming <aliayoub22@gmail.com>
2015-01-28 21:45:27 -0500

    bubble_sort_by added

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -9,7 +9,25 @@ def bubble_sort(array)
             a[i], a[i+1] = a[i+1], a[i]
             swapped = true
         end
+        end
     end
+
+    return a
+
+end
+
+def bubble_sort_by(array)
+
+    a = array
+    swapped = true
+    while swapped do
+        swapped = false
+        0.upto(a.size-2) do |i|
+        if yield(a[i], a[i+1]) > 0
+            a[i], a[i+1] = a[i+1], a[i]
+            swapped = true
+        end
+        end
     end
 
     return a
@@ -17,3 +35,5 @@ def bubble_sort(array)
 end
 
 bubble_sort([1,2,3,4,5,6,7,3,4,3,2,5,7])
+
+bubble_sort_by(["hey", "hello", "hi"])
